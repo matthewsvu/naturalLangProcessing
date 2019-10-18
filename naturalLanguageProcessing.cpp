9470f3fd4120ba46f5439f716ba722be4f080c77
Matthew Vu <MSV180000@utdallas.edu>
2019-10-18 16:17:37 -0500

    Add files via upload

---
+++ naturalLanguageProcessing.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+
+
+using namespace std;
+
+int main()
+{
+    double num= 0, maxNum = 0, average, total= 0, minNum= 100;
+    int count = 0;
+    string input, type, exception;
+    getline(cin, input);
+    for(int i = 0; i < input.length(); i++)
+    {
+
+        istringstream sinput(input);
+        while(sinput >> input)
+        {
+
+            if(isdigit(input[0]))
+            {
+                num = stod(input);
+                cout << num << " ";
+                total += num;
+                maxNum = max(num, maxNum);
+                minNum = min(num, minNum);
+                average = total;
+                count++;
+            }
+            else if(input.find("bye") == true || input.find("sleep.") == true )
+            {
+                cout << endl;
+                cout << "OK bye!";
+                break;
+            }
+            else if(input == "total")
+            {
+                type = input;
+            }
+            else if(input == "max")
+            {
+                type = input;
+            }
+            else if(input == "min")
+            {
+                type = input;
+            }
+            else if(input == "average")
+            {
+                type = input;
+            }
+            else
+            {
+                exception = exception + input + " ";
+            }
+        }
+        if(input == "bye" || input == "sleep.")
+        {
+            cout << endl;
+            cout << "OK bye!";
+            break;
+        }
+        else if(type == "total")
+        {
+            cout << type << " " << total << endl;
+        }
+        else if(type == "max")
+        {
+            cout << type << " " << maxNum << endl;
+        }
+        else if(type == "min")
+        {
+            cout << type << " " << minNum << endl;
+        }
+        else if(type == "average")
+        {
+            cout << type << " " << average / count << endl;
+        }
+        else
+        {
+            cout << exception << " - I am not yet that smart to answer this one!" << endl;
+
+        }
+        average = 0;
+        total = 0;
+        maxNum = 0;
+        minNum = 100;
+        type = "";
+        count = 0;
+        exception = "";
+        getline(cin, input);
+
+    }
+}
